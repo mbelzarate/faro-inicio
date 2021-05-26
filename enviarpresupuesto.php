4c9c87df91686a71461f70c7191fb71ce065f390
mbelzarate <84828396+mbelzarate@users.noreply.github.com>
2021-05-26 19:31:57 -0300

    Add files via upload

--- enviarpresupuesto.php
+++ enviarpresupuesto.php
@@ -18,4 +18,52 @@ if (isset($_POST['enviar'])) {
 
     echo '<div>Presupuestar los servicios: '.$selected.'</div>';
 }    
+
+
+
+if (empty($_POST["nombre"])) {
+    exit("Falta el nombre");
+}
+
+if (empty($_POST["apellido"])) {
+    exit("Falta el apellido");
+}
+
+if (empty($_POST["correo"])) {
+    exit("Falta el correo electronico");
+}
+
+
+
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$correo = $_POST["correo"];
+
+
+$correo = filter_var($correo, FILTER_VALIDATE_EMAIL);
+if (!$correo) {
+    echo "Correo inválido. Intenta con otro correo.";
+    exit;
+}
+
+$asunto = "Nuevo mensaje de sitio web";
+
+$datos = "De: $nombre\nCorreo: $correo\napellido: $apellido";
+$mensaje = "Has recibido un mensaje desde el formulario de contacto de tu sitio web. Aquí están los detalles:\n$datos";
+
+$destinatario = "creativos.faro@gmail.com"; # aquí la persona que recibirá los mensajes
+
+$encabezados .= "De: $nombre <" . $correo . ">\r\n";
+$encabezados .= "Reply-To: $nombre <$correo>\r\n";
+$resultado = mail($destinatario, $asunto, $mensaje, $encabezados);
+
+
+if ($resultado) {
+    echo "<h1>Mensaje enviado, ¡Gracias por contactarnos!</h1>";
+    echo "<p>Tu mensaje se ha enviado correctamente.</p><h2>Importante</h2><p>Revisa tu bandeja de SPAM, en ocasiones mis respuestas quedan ahí. </p>";
+} else {
+    echo "Tu mensaje no se ha enviado. Intenta de nuevo.";
+}
+
+
 ?>
