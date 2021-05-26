f55ff617756fef59247bf800d0472ae4ddc36ef6
mbelzarate <84828396+mbelzarate@users.noreply.github.com>
2021-05-26 19:34:11 -0300

    Add files via upload

--- enviarpresupuesto.php
+++ enviarpresupuesto.php
@@ -1,25 +1,4 @@
 <?php
-if (isset($_POST['enviar'])) {
-    if (is_array($_POST['servicio'])) {
-        $selected = '';
-        $num_servicio = count($_POST['servicio']);
-        $current = 0;
-        foreach ($_POST['servicio'] as $key => $value) {
-            if ($current != $num_servicio-1)
-                $selected .= $value.', ';
-            else
-                $selected .= $value.'.';
-            $current++;
-        }
-    }
-    else {
-        $selected = 'Debes seleccionar al menos un servicio';
-    }
-
-    echo '<div>Presupuestar los servicios: '.$selected.'</div>';
-}    
-
-
 
 if (empty($_POST["nombre"])) {
     exit("Falta el nombre");
@@ -64,6 +43,27 @@ if ($resultado) {
 } else {
     echo "Tu mensaje no se ha enviado. Intenta de nuevo.";
 }
+
+
+if (isset($_POST['enviar'])) {
+    if (is_array($_POST['servicio'])) {
+        $selected = '';
+        $num_servicio = count($_POST['servicio']);
+        $current = 0;
+        foreach ($_POST['servicio'] as $key => $value) {
+            if ($current != $num_servicio-1)
+                $selected .= $value.', ';
+            else
+                $selected .= $value.'.';
+            $current++;
+        }
+    }
+    else {
+        $selected = 'Debes seleccionar al menos un servicio';
+    }
+
+    echo '<div>Presupuestar los servicios: '.$selected.'</div>';
+}    
 
 
 ?>
